26df987c7bfab10baeda24650b5a6933a14e98d1
Appoline <37341396+Appoline@users.noreply.github.com>
2018-12-18 18:21:47 +0100

    Add files via upload

--- bulletin.js
+++ bulletin.js
@@ -1,79 +1,97 @@
 var hydraExpress = require('hydra-express');
 var hydra = hydraExpress.getHydra();
 var config = require('./config.json');
-
+var bulletins = new Array();
 function getRandomInt(max) {
   
    return Math.floor(Math.random() * Math.floor(max));
 
 }
 
-randomMeteo = getRandomInt(5);
+function genererBulletins(nbrBulletins){
+	let bulletin = {
+			date : null,
+			meteo : null,
+			temperature : null,
+			ville : null
+	}; 
+	for (i=0 ; i<nbrBulletins ; i++){
+		randomMeteo = getRandomInt(5);
+		switch(randomMeteo) {
+		case 0:
+			meteo = "Brouillard givrant";
+			break;
+		case 1:
+			meteo = "Grand beau temps";
+			break;
+		case 2:
+			meteo = "Pluie";
+			break;
+		case 3:
+			meteo = "Vent fort";
+			break;
+		case 4:
+			meteo = "Nuageux";
+		}
+		bulletin.meteo=randomMeteo;
+		
+		randomTemp = getRandomInt(4);
 
-switch(randomMeteo) {
-    case 0:
-        meteo = "Brouillard givrant";
-        break;
-    case 1:
-        meteo = "Grand beau temps";
-        break;
-    case 2:
-        meteo = "Pluie";
-        break;
-    case 3:
-        meteo = "Vent fort";
-        break;
-    case 4:
-        meteo = "Nuageux";
-}
+		switch(randomTemp) {
+			case 0:
+				temperature = "Chaud";
+				break;
+			case 1:
+				temperature = "Froid";
+				break;
+			case 2:
+				temperature = "De saison";
+				break;
+			case 3:
+				temperature = "Doux";
+		}
+		bulletin.temperature=randomTemp;
+		
+		randomVille = getRandomInt(4);
 
-randomTemp = getRandomInt(4);
+		switch(randomVille) {
+			case 0:
+				ville = "Annecy";
+				break;
+			case 1:
+				ville = "Lyon";
+				break;
+			case 2:
+				ville = "Paris";
+				break;
+			case 3:
+				ville = "Chambery";
+		}
+		bulletin.ville=randomVille;
+		
+		date = new Date();
+		bulletin.date=date;
+	
+		bulletins.push(bulletin);
+	}
 
-switch(randomTemp) {
-    case 0:
-        meteo += " - Chaud";
-        break;
-    case 1:
-        meteo += " - Froid";
-        break;
-    case 2:
-        meteo += " - De saison";
-        break;
-    case 3:
-        meteo += " - Doux";
-}
-
-randomGeo = getRandomInt(4);
-
-switch(randomGeo) {
-    case 0:
-        meteo += " - Annecy";
-        break;
-    case 1:
-        meteo += " - Lyon";
-        break;
-    case 2:
-        meteo += " - Paris";
-        break;
-    case 3:
-        meteo += " - Chambery";
-}
-
-date = new Date();
-
-hydraExpress.init(config, () => {})
-  .then((serviceInfo) => {
-    console.log('serviceInfo', serviceInfo);
-    hydra.on('message', (message) => {
-      let messageReply = hydra.createUMFMessage({
-        to: 'modif:/',
-        frm: 'bulletin:/',
-        bdy: {
-          msg: `Bulletin meteo du ${date} - ${meteo}`
-        }
-      });
-      hydra.sendMessage(messageReply);
-    });
-    return 0;
-  })
-  .catch(err => console.log('err', err));
+	hydraExpress.init(config, () => {})
+	  .then((serviceInfo) => {
+		console.log('serviceInfo', serviceInfo);
+		hydra.on('message', (message) => {
+		  let messageReply = hydra.createUMFMessage({
+			to: 'modif:/',
+			frm: 'bulletin:/',
+			bdy: bulletins
+		  });
+		  hydra.sendMessage(messageReply);
+		});
+		return 0;
+	  })
+	  .catch(err => console.log('err', err));
+	  
+ }
+ 
+ //manque à appeler la fonction 
+ var nbBull = readline.question("Combien de bulletins ajoutez-vous ?");
+ genererBullletins(nbBull);
